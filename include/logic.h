ba2a240b15cb39ae088aa362029f1dcb0b476071
briascoleonel <leonelbriasco@mi.unc.edu.ar>
2024-03-22 14:40:03 +0400

    Se agregan la linea al final

--- include/logic.h
+++ include/logic.h
@@ -4,4 +4,5 @@ int add(int first_number, int second_number);
 int subtract(int first_number, int second_number);
 int multiply(int first_number, int second_number);
 double divide(int first_number, int second_number);
+
    
